8acbf149f4e3130a131c78b6bf5768858296a8e2
Hydraxous <robo-trey@outlook.com>
2024-01-11 09:44:42 -0800

    Update Paths.cs

--- src/CultOfJakito.UltraTelephone2/Data/Paths.cs
+++ src/CultOfJakito.UltraTelephone2/Data/Paths.cs
@@ -10,11 +10,12 @@ namespace CultOfJakito.UltraTelephone2
 
         const string BUNDLE_FOLDER_NAME = "Bundles";
         const string DATA_FILE_NAME = "saveData.json";
+        const string DATA_FOLDER_NAME = "Data";
 
         public static string BundleFolder => Path.Combine(ModFolder, BUNDLE_FOLDER_NAME);
         public static string ModFolder => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         public static string DataFilePath => Path.Combine(DataFolder, DATA_FILE_NAME);
-        public static string DataFolder => Path.Combine(ModFolder, "Data");
+        public static string DataFolder => Path.Combine(ModFolder, DATA_FOLDER_NAME);
 
         public static string GetBundleFilePath(string bundleFileName)
         {
