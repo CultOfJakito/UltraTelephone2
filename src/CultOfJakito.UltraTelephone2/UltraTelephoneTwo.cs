253f9af4f8c767780c0ee3436ec3cdf10637be2c
Waffle <60797216+wafflethings@users.noreply.github.com>
2024-01-14 21:38:57 +0000

    move files

--- src/CultOfJakito.UltraTelephone2/UltraTelephoneTwo.cs
+++ src/CultOfJakito.UltraTelephone2/UltraTelephoneTwo.cs
@@ -11,7 +11,7 @@ using UnityEngine.SceneManagement;
 namespace CultOfJakito.UltraTelephone2;
 
 [BepInDependency("Hydraxous.ULTRAKILL.Configgy", BepInDependency.DependencyFlags.HardDependency)]
-[BepInPlugin(nameof(CultOfJakito.UltraTelephone2), "UltraTelephone 2", "1.0.0")]
+[BepInPlugin(nameof(UltraTelephone2), "UltraTelephone 2", "1.0.0")]
 public class UltraTelephoneTwo : BaseUnityPlugin
 {
     public AssetLoader AssetLoader { get; private set; }
@@ -28,26 +28,26 @@ public class UltraTelephoneTwo : BaseUnityPlugin
     {
         Instance = this;
 
-        _config = new ConfigBuilder(nameof(CultOfJakito.UltraTelephone2), "Ultra Telephone 2");
+        _config = new ConfigBuilder(nameof(UltraTelephone2), "Ultra Telephone 2");
         _config.Build();
 
         Paths.ValidateFolders();
         InGameCheck.Init();
 
-		new Harmony(Info.Metadata.GUID).PatchAll(Assembly.GetExecutingAssembly());
+        new Harmony(Info.Metadata.GUID).PatchAll(Assembly.GetExecutingAssembly());
 
         string username = Environment.UserName;
         int dayOfTheWeek = (int)DateTime.Now.DayOfWeek;
 
-        Random = new System.Random(username.GetHashCode()+dayOfTheWeek);
+        Random = new System.Random(username.GetHashCode() + dayOfTheWeek);
 
         ZelzmiyBundle = new(Paths.GetBundleFilePath("Zelzmiy.resource"));
 
         InGameCheck.OnLevelChanged += DoThing;
-		SceneManager.sceneLoaded += OnSceneLoaded;
-	}
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
 
-	private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         if (SceneManager.GetActiveScene() != scene)
             return;
@@ -55,7 +55,7 @@ public class UltraTelephoneTwo : BaseUnityPlugin
         if (!InGameCheck.InLevel())
             return;
 
-		Logger.LogInfo("Starting new service scope");
+        Logger.LogInfo("Starting new service scope");
         GameObject chaosManagerObject = new GameObject("UT2 Chaos Manager");
         ChaosManager = chaosManagerObject.AddComponent<ChaosManager>();
 
@@ -68,10 +68,6 @@ public class UltraTelephoneTwo : BaseUnityPlugin
             return;
 
         InGameCheck.OnLevelChanged -= DoThing;
-        ModalDialogue.ShowSimple("ULTRATELEPHONE", "ULTRA TELEPHONE", (b) =>
-        {
-
-        }, "No?", "FUCK NO");
-
+        ModalDialogue.ShowSimple("ULTRATELEPHONE", "ULTRA TELEPHONE", (b) => { }, "No?", "FUCK NO");
     }
 }
