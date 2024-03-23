b748a5c1fd330cf5fa705f9658fd67702c515e65
Hydraxous <robo-trey@outlook.com>
2024-03-23 08:03:47 -0700

    port herobrine

---
+++ src/CultOfJakito.UltraTelephone2/Hydra/Herobrine/HerobrineManager.cs
@@ -0,0 +1,57 @@
+﻿using Configgy;
+using CultOfJakito.UltraTelephone2.Assets;
+using UnityEngine;
+
+namespace UltraTelephone.Hydra
+{
+    public static class HerobrineManager
+    {
+        private static Herobrine herobrine;
+        private static GameObject herobrinePrefab => UT2Assets.UltraTelephoneLegacyBundle.LoadAsset<GameObject>("Herobrine");
+
+        [Configgable("Hydra/Fun/Herobrine", "Herobrine Enabled")]
+        private static ConfigToggle herobrineEnabled = new ConfigToggle(true);
+
+        private static bool initialized;
+
+        public static void Init()
+        {
+            if (initialized)
+                return;
+
+            initialized = true;
+            herobrineEnabled.OnValueChanged += SetHerobrineEnabled;
+            SetHerobrineEnabled(herobrineEnabled.Value);
+        }
+
+        private static void SetHerobrineEnabled(bool enabled)
+        {
+            if (enabled)
+            {
+                if (herobrine == null)
+                {
+                    SpawnHerobrine();
+                }
+                else
+                {
+                    herobrine.gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                if (herobrine != null)
+                {
+                    herobrine.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        private static void SpawnHerobrine()
+        {
+            GameObject herobrineGO = GameObject.Instantiate(herobrinePrefab);
+            herobrine = herobrineGO.GetComponent<Herobrine>();
+            GameObject.DontDestroyOnLoad(herobrineGO);
+            Debug.Log("SPAWNED HEROBRINE");
+        }
+    }
+}
