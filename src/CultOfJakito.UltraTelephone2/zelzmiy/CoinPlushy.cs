9732eb669ee4a2eb8622af45bd60d8a6244109e6
zelzmiy <51797223+zelzmiy@users.noreply.github.com>
2024-03-29 14:20:06 -0500

    Add CoinPlushy Chaos effect + dev plushies from uc
    oh fuck i forgot to public this branch

---
+++ src/CultOfJakito.UltraTelephone2/zelzmiy/CoinPlushy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Configgy;
+using CultOfJakito.UltraTelephone2.Assets;
+using CultOfJakito.UltraTelephone2.Chaos;
+using CultOfJakito.UltraTelephone2.DependencyInjection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CultOfJakito.UltraTelephone2.zelzmiy
+{
+    [RegisterChaosEffect]
+    internal class CoinPlushy : ChaosEffect
+    {
+
+        [Configgable("Chaos/Effects", "Coin Plushies")]
+        private static ConfigToggle s_enabled = new(true);
+
+        private static bool s_effectActive;
+
+        private static List<GameObject> _plushiePrefabs = new()
+        {
+            UT2Assets.ZelzmiyBundle.LoadAsset<GameObject>("estrogen burger.prefab"),
+        };
+
+        private static int _randomPlushieIndex;
+
+        public override void BeginEffect(UniRandom random)
+        {
+            _randomPlushieIndex = random.Next(0, 1);
+            s_effectActive = true;
+        }
+
+        public override int GetEffectCost() => 1;
+
+        [HarmonyPatch(typeof(Coin), "Start"), HarmonyPostfix]
+        private static void ChangeCoinToPlushie(Coin __instance)
+        {
+            if (!s_enabled.Value || !s_effectActive)
+                return;
+
+            GameObject plushie = _plushiePrefabs[_randomPlushieIndex];
+
+            GameObject plush = Instantiate(plushie,__instance.transform.position,__instance.transform.rotation);
+            plush.GetComponent<Rigidbody>().AddForce(__instance.GetComponent<Rigidbody>().velocity, ForceMode.VelocityChange);
+            
+            Destroy(__instance.gameObject);
+        }
+    }
+}
