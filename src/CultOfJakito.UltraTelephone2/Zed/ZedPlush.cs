83f11260912b145fda1aed9052b01d6530aa4620
zelzmiy <51797223+zelzmiy@users.noreply.github.com>
2024-03-30 17:01:56 -0500

    waffle tex fix + *Very* *very* broken zed

---
+++ src/CultOfJakito.UltraTelephone2/Zed/ZedPlush.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ZedPlush : MonoBehaviour
+{
+    [SerializeField]
+    private Texture2D[] _faces;
+    [SerializeField]
+    private Material _screenMaterial;
+
+    public void ChangeFace()
+    {
+        _screenMaterial.mainTexture = _faces[UnityEngine.Random.Range(0, _faces.Length)];
+    }
+}
