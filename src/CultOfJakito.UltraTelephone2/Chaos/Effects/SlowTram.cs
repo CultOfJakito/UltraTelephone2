e5ab01555e77d8e99fc7d3e4eeca5c427eda453c
Yuvraj Verma <vermayuvraj2010@gmail.com>
2024-03-30 19:23:01 +0530

    Create SlowTram.cs

---
+++ src/CultOfJakito.UltraTelephone2/Chaos/Effects/SlowTram.cs
@@ -0,0 +1,29 @@
+using Configgy;
+using CultOfJakito.UltraTelephone2.DependencyInjection;
+using HarmonyLib;
+using Train;
+
+namespace CultOfJakito.UltraTelephone2.Chaos.Effects;
+
+[RegisterChaosEffect]
+[HarmonyPatch]
+public class SlowTram : ChaosEffect
+{
+    [Configgable("Chaos/Effects", "Tram Speed")]
+    private static ConfigToggle s_enabled = new(true);
+
+    private static bool s_currentlyActive;
+
+    public override void BeginEffect(UniRandom random) => s_currentlyActive = true;
+    public override bool CanBeginEffect(ChaosSessionContext ctx) => s_enabled.Value && base.CanBeginEffect(ctx);
+    public override int GetEffectCost() => 1;
+
+    [HarmonyPatch(typeof(TramControl), nameof(TramControl.UpdateSpeedIndicators)), HarmonyPostfix]
+    public static void SpeedLimit(TramControl __instance)
+    {
+        if (!s_currentlyActive && !s_enabled.Value)
+            return;
+        __instance.speedMultiplier = 10;
+        __instance.targetTram.speed = 0;
+    }
+}
