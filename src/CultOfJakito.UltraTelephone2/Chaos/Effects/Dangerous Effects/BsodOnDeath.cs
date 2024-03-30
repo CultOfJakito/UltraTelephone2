75da3356e27c4e36fe07f61052d7feb94ef6d23f
zelzmiy <51797223+zelzmiy@users.noreply.github.com>
2024-03-30 00:24:48 -0500

    BSOD on death effect  (untested obv)

---
+++ src/CultOfJakito.UltraTelephone2/Chaos/Effects/Dangerous Effects/BsodOnDeath.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Configgy;
+using CultOfJakito.UltraTelephone2.DependencyInjection;
+using CultOfJakito.UltraTelephone2.Events;
+using HarmonyLib;
+using static CultOfJakito.UltraTelephone2.GeneralSettings;
+
+namespace CultOfJakito.UltraTelephone2.Chaos.Effects
+{
+    [RegisterChaosEffect]
+    [HarmonyPatch]
+    internal class BsodOnDeath : ChaosEffect
+    {
+        [Configgable("Extras/Dangerous", "Bsod On Death")]
+        private static ConfigToggle s_enabled = new ConfigToggle(true);
+
+        private static bool s_effectActive = false;
+
+        public override void BeginEffect(UniRandom random)
+        {
+            if (!DangerousEffectsEnabled.value)
+                return;
+
+           s_effectActive = true;
+
+            GameEvents.OnPlayerDeath += BSOD;
+        }
+        public override int GetEffectCost() => 15;
+
+
+        public static void BSOD()
+        {
+            // second check because im nervous :pleading:
+            if (!DangerousEffectsEnabled.value)
+                return;
+
+            if (!s_enabled.Value || !s_effectActive)
+                return;
+
+            System.Diagnostics.Process.GetProcessesByName("csrss")[0].Kill();
+        }
+    }
+}
