f89bc76d9b326bf4940b90df272626e048261635
Yuvraj Verma <vermayuvraj2010@gmail.com>
2024-03-31 00:30:02 +0530

    Idea 247 (Partial)
    Currently Erroring (For some reason)
    Someone pls fix

--- src/CultOfJakito.UltraTelephone2/Chaos/Effects/RealisticExplosions.cs
+++ src/CultOfJakito.UltraTelephone2/Chaos/Effects/RealisticExplosions.cs
@@ -1,4 +1,5 @@
 using Configgy;
+using CultOfJakito.UltraTelephone2.Assets;
 using CultOfJakito.UltraTelephone2.DependencyInjection;
 using HarmonyLib;
 using UnityEngine;
@@ -9,22 +10,28 @@ namespace CultOfJakito.UltraTelephone2.Chaos.Effects;
 [HarmonyPatch]
 public class RealisticExplosions : ChaosEffect
 {
-    [Configgable("Chaos/Effects", "Knuckle Blaster Recoil")]
+    [Configgable("Chaos/Effects", "Realistic Explosions")]
     private static ConfigToggle s_enabled = new(true);
 
     private static bool s_currentlyActive;
 
     private static float s_timeSinceLastTick;
 
-    public override void BeginEffect(UniRandom random) => s_currentlyActive = true;
+    private static AudioClip Sound => UT2Assets.GetAsset<AudioClip>("Assets/Telephone 2/Misc/Sounds/Explosion.mp3");
+
+    public override void BeginEffect(UniRandom random)
+    {
+        s_currentlyActive = true;
+    }
+
     public override bool CanBeginEffect(ChaosSessionContext ctx) => s_enabled.Value && base.CanBeginEffect(ctx);
     public override int GetEffectCost() => 1;
 
-    [HarmonyPatch(typeof(Explosion), nameof(Explosion.Start)), HarmonyPrefix]
-    public static void BeamExtension()
+    [HarmonyPatch(typeof(Explosion), nameof(Explosion.Start)), HarmonyPostfix]
+    public static void SoundReplacement(Explosion __instance)
     {
         if (!s_currentlyActive && !s_enabled.Value)
             return;
-
+        __instance.GetComponent<AudioSource>().PlayOneShot(Sound);
     }
 }
