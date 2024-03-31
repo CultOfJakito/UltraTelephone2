baec412b485cd8a28dacc5b51918b6100f222ce5
Hydraxous <robo-trey@outlook.com>
2024-03-31 12:34:16 -0700

    make crushers worse

--- src/CultOfJakito.UltraTelephone2/Patches/WeirdCrushers.cs
+++ src/CultOfJakito.UltraTelephone2/Patches/WeirdCrushers.cs
@@ -26,29 +26,76 @@ namespace CultOfJakito.UltraTelephone2.Patches
                 .WithSeed(__instance.transform.position));
 
             //Minimum return time needed to be able to extend fully
+
+            if(random.Chance(0.2f))
+            {
+                if(!__instance.TryGetComponent<MegaAssholeCrusher>(out MegaAssholeCrusher _))
+                {
+                    __instance.gameObject.AddComponent<MegaAssholeCrusher>();
+                }
+            }
+            else
+            {
+                __instance.RandomizePiston(random);
+            }
+
+        }
+
+        public static void RandomizePiston(this Piston __instance, UniRandom random)
+        {
             const float minReturnTime = 0.18f;
 
             //Fast return or slow return
-            __instance.returnTime = (random.Bool()) ? random.Range(minReturnTime, minReturnTime*1.5f) : random.Range(2f, 4f);
-            __instance.attackTime = (random.Bool()) ? random.Range(0.05f,0.5f) : random.Range(3f, 6f);
+            __instance.returnTime = (random.Bool()) ? random.Range(minReturnTime, minReturnTime * 1.5f) : random.Range(2f, 4f);
+            __instance.attackTime = (random.Bool()) ? random.Range(0.05f, 0.5f) : random.Range(3f, 6f);
 
             //random chance to be harmless...
-            if(random.Chance(0.05f))
+            if (random.Chance(0.05f))
             {
-                if(__instance.dzone is BoxCollider)
+                if (__instance.dzone is BoxCollider)
                 {
                     DeathZone dz = __instance.dzone.GetComponent<DeathZone>();
-                    if(dz != null)
+                    if (dz != null)
                     {
                         UnityEngine.Object.Destroy(dz);
                     }
                 }
 
-            }else if (random.Chance(0.05f)) //Random chance to be super evil....
+            }
+            else if (random.Chance(0.05f)) //Random chance to be super evil....
             {
                 __instance.returnTime = minReturnTime;
                 __instance.attackTime = 0f;
             }
+        }
+    }
+
+    public class MegaAssholeCrusher : MonoBehaviour
+    {
+        private Piston piston;
+        private UniRandom random;
+        float changeTick;
+        private void Awake()
+        {
+            piston = GetComponent<Piston>();
+            random = new UniRandom(new SeedBuilder()
+                .WithGlobalSeed()
+                .WithSceneName()
+                .WithSeed(piston.transform.position));
+
+            changeTick = random.Range(0.4f, 4f);
+        }
+
+        private void SlowChange()
+        {
+            if(piston == null)
+            {
+                Destroy(this);
+                return;
+            }
+
+            piston.RandomizePiston(random);
+            Invoke(nameof(SlowChange), changeTick);
         }
     }
 }
