6a030c692dd52f37fd637970946a69789cab416d
Hydraxous <robo-trey@outlook.com>
2024-04-12 23:22:36 -0700

    Book placeholder parsing

---
+++ src/CultOfJakito.UltraTelephone2/Patches/BookPlaceholderParser.cs
@@ -0,0 +1,17 @@
+﻿using CultOfJakito.UltraTelephone2.Placeholders;
+using HarmonyLib;
+
+namespace CultOfJakito.UltraTelephone2.Patches
+{
+    [HarmonyPatch]
+    public static class BookPlaceholderParser
+    {
+
+        [HarmonyPatch(typeof(ScanningStuff), nameof(ScanningStuff.ScanBook)), HarmonyPrefix]
+        [HarmonyPriority(100)]
+        private static void OnScanBook(ScanningStuff __instance, ref string text)
+        {
+            text = PlaceholderHelper.ReplacePlaceholders(text);
+        }
+    }
+}
